fd71d08325158c767c5b5b116c36111c9b831f6d
Tim Harper <timcharper@gmail.com>
2009-07-26 17:26:44 -0600

    prefer constant over instance variable

--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -1,8 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Directory do
-  before(:each) do
-    @example_listing = <<-EOF    
+  EXAMPLE_LISTING = <<-EOF
 Household	Given Name	Sex	Birth Date	Age
 Charleson	Dixie T	F	05 Mar	
       16 S. Village Court Road	 	 	 	 
@@ -12,16 +11,20 @@ Chewey	Sue	F	04 Oct
       Saratoga Springs, UT  84045	 	 	 	 
       801-756-1768	 	 	 	 
 EOF
-  end
   
   it "parses a block of families" do
-    directory = Directory.parse(@example_listing)
+    directory = Directory.parse(EXAMPLE_LISTING)
     directory.should have(2).families
     directory.families.map(&:surname).should == %w[Charleson Chewey]
   end
   
+  it "skips over 'see such and such' lines" do
+    directory = Directory.parse(EXAMPLE_LISTING + "Person, Awesome, see Chewey, Sue\n")
+    directory.should have(2).families
+  end
+
   it "matches a list of photos to families" do
-    directory = Directory.parse(@example_listing)
+    directory = Directory.parse(EXAMPLE_LISTING)
     unused_photos = directory.match_photos(["/photos/Charleson, Dixie.jpg", "/photos/Chewey, Sue.jpg", "/photos/Guy, Some.jpg"])
     unused_photos.should == ["/photos/Guy, Some.jpg"]
     directory.families[0].photo.should == "/photos/Charleson, Dixie.jpg"
@@ -29,7 +32,7 @@ EOF
   end
   
   it "matches a list of photos from in a folder to families" do
-    directory = Directory.parse(@example_listing)
+    directory = Directory.parse(EXAMPLE_LISTING)
     Dir.should_receive(:[]).with('/folder/*').and_return(["/photos/Charleson, Dixie.jpg", "/photos/Chewey, Sue.Jpg", "/photos/Guy, Some.JPEG"])
     unused_photos = directory.match_photos('/folder')
     unused_photos.should == ["/photos/Guy, Some.JPEG"]
